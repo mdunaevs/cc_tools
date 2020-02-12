c979e4698bf5348071ce16f5ad40fe281d680a7d
Max <mdunaevs@andrew.cmu.edu>
2020-02-12 13:26:58 -0500

    Finished running test to make sure that I could setup and test TilWorld

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -1,8 +1,8 @@
-import cc_dat_utils
-
+from cc_dat_utils import *
 #Part 1
 input_dat_file = "data/pfgd_test.dat"
 
 
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
 #print the resulting data
+print(make_cc_level_pack_from_dat(input_dat_file))
